46b45ef3c338c12423ad2eff2b839fba69481860
Doyun <materokatti@gmail.com>
2024-08-08 09:13:37 +0900

    feat : prepare view model

--- app/src/main/java/com/kodeco/android/viewmodel/ListDataManager.kt
+++ app/src/main/java/com/kodeco/android/viewmodel/ListDataManager.kt
@@ -1,18 +1,22 @@
 package com.kodeco.android.viewmodel
 
+import android.app.Application
 import android.content.Context
+import androidx.lifecycle.AndroidViewModel
 import androidx.preference.PreferenceManager
 import com.kodeco.android.data.TaskList
+import java.lang.Appendable
 
-class ListDataManager(private val context: Context) {
+class ListDataManager(private val application: Application): AndroidViewModel(application) {
+
     fun saveList(list: TaskList) {
-        val sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context).edit()
+        val sharedPrefs = PreferenceManager.getDefaultSharedPreferences(application).edit()
         sharedPrefs.putStringSet(list.name, list.tasks.toHashSet())
         sharedPrefs.apply()
     }
 
     fun readLists(): ArrayList<TaskList> {
-        val sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context)
+        val sharedPrefs = PreferenceManager.getDefaultSharedPreferences(application)
         val contents = sharedPrefs.all
         val taskLists = ArrayList<TaskList>()
         for (taskList in contents) {
