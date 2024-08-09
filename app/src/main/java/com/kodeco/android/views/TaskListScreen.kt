b87a2f001ea39480717e6a31fbf70e8c33188327
Doyun <materokatti@gmail.com>
2024-08-09 10:05:07 +0900

    feat : create viewmodel

--- app/src/main/java/com/kodeco/android/views/TaskListScreen.kt
+++ app/src/main/java/com/kodeco/android/views/TaskListScreen.kt
@@ -7,11 +7,14 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.kodeco.android.R
 import com.kodeco.android.data.TaskList
+import com.kodeco.android.viewmodel.ListDataManager
 
 @Composable
 fun TaskListScreen() {
+    val taskListViewModel: ListDataManager = viewModel()
     Scaffold(
         topBar = {
             ListMakerTopAppBar(
