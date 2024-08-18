7aeae0da868c13a74dca0d862c271c73e106dd32
Doyun <materokatti@gmail.com>
2024-08-19 08:20:01 +0900

    feat : complete TaskDetailsScreenComposable

--- app/src/main/java/com/kodeco/android/views/TasksDetailScreen.kt
+++ app/src/main/java/com/kodeco/android/views/TasksDetailScreen.kt
@@ -1,11 +1,14 @@
 package com.kodeco.android.views
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import com.kodeco.android.R
+import com.kodeco.android.navigation.Screens
 
 @Composable
 fun TaskDetailScreenContent(
@@ -29,4 +32,34 @@ fun TaskDetailScreenContent(
             }
         )
     }
+}
+
+@Composable
+fun TaskDetailScreen(){
+    Scaffold(
+        topBar = {
+            ListMakerTopAppBar(
+                title = "",
+                showBackButton = true,
+                onBackPressed = {
+
+                }
+            )
+        },
+        content = {
+            TaskDetailScreenContent(
+                modifier = Modifier.padding(it),
+                taskTodos = emptyList()
+            )
+        },
+        floatingActionButton = {
+            ListMakerFloatingActionButton(
+                title = stringResource(id = R.string.task_to_add),
+                inputHint = stringResource(id = R.string.task_hint),
+                onFabClick = { todoName ->
+
+                }
+            )
+        }
+    )
 }
